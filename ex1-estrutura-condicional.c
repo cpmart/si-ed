a6ad1b8f8028c2de331c03c41a9036c82f307964
cpmart <cpmart@gmail.com>
2023-08-25 20:10:41 -0300

    ex1-if

---
+++ ex1-estrutura-condicional.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    float temperatura;
+    printf("Digite a temperatura: ");
+    scanf("%f",&temperatura);
+    if(temperatura<20)
+        printf("Frio\n");
+    else if(temperatura<30)
+        printf("Agradável\n");
+    else
+        printf("Quente\n");
+        
+
+}
