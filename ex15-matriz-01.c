ef62f1811499f4d4412c52c07530a0aa81dd5657
cpmart <cpmart@gmail.com>
2023-10-06 20:59:46 -0300

    matriz2

--- ex15-matriz-01.c
+++ ex15-matriz-01.c
@@ -9,19 +9,15 @@ int main()
     int matriz[L][C], totalLinha = 0, vetor[C] = {};
     srand(time(NULL));
     // Preenchendo a matriz
-    for (int i = 0; i < L; i++)
-    {
-        for (int j = 0; j < C; j++)
-        {
+    for (int i = 0; i < L; i++){
+        for (int j = 0; j < C; j++){
             matriz[i][j] = rand() % 16;
         }
     }
     // Mostrando a matriz e o total por linha
-    for (int i = 0; i < L; i++)
-    {
+    for (int i = 0; i < L; i++){
         totalLinha = 0;
-        for (int j = 0; j < C; j++)
-        {
+        for (int j = 0; j < C; j++){
             printf("  %2d", matriz[i][j]);
             totalLinha += matriz[i][j];
             vetor[j] += matriz[i][j];
@@ -31,8 +27,7 @@ int main()
     }
     printf("----------------------\n");
     // Mostrando total por linha da matriz
-    for (int j = 0; j < C; j++)
-    {
+    for (int j = 0; j < C; j++){
         printf("  %2d", vetor[j]);
     }
     printf("\n\n");
